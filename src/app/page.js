7c355f5d82a9957627fb1e7126601f62a2d566d0
Teknofilia <116430893+Teknofilia@users.noreply.github.com>
2024-01-02 23:00:31 +0700

    fix: bug fix error merging from api-schema branch

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,21 @@
+import { Header } from "@/components/sharedUI/header";
+import { apiUrl } from "@/config/apiUrl";
+
+async function getData(query) {
+  if (!query) {
+    const res = await fetch(`${apiUrl}/products`);
+    const data = await res.json();
+    return data;
+  }
+
+  const res = await fetch(`${apiUrl}/products?q=${query}`);
+  const data = await res.json();
+  return data;
+}
 export default function Home() {
-  return <main>Hey There</main>;
+  return (
+    <main className="max-w-5xl m-auto py-8 space-y-20">
+      <Header />
+    </main>
+  );
 }
