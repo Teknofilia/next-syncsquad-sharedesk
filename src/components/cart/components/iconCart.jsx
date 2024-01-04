ccec8f5ca241d0d29e168b58ab4d3f4a622f4046
anto1313 <anthorouw@gmail.com>
2024-01-05 01:03:33 +0700

    fix iconcart

--- src/components/cart/components/iconCart.jsx
+++ src/components/cart/components/iconCart.jsx
@@ -6,7 +6,7 @@ import { useAtom } from "jotai";
 
 
 export const iconCart = () => {
-    const [cart, setCart] useAtom(cartAtom);
+    const [cart, setCart] = useAtom(cartAtom);
   return (
     <Badge content={cart} color="danger">
     <ShoppingBag size={24} />
