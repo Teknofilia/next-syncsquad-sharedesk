680fe1b985583773fe04766f5b5cd40e29efaefe
anto1313 <anthorouw@gmail.com>
2024-01-05 00:46:19 +0700

    feat: add atom

---
+++ src/components/cart/components/iconCart.jsx
@@ -0,0 +1,15 @@
+"use client"
+import { Badge } from "@nextui-org/react";
+import { ShoppingBag } from "lucide-react";
+import { cartAtom } from "../atom/cartAtom";
+import { useAtom } from "jotai";
+
+
+export const iconCart = () => {
+    const [cart, setCart] useAtom(cartAtom);
+  return (
+    <Badge content={cart} color="danger">
+    <ShoppingBag size={24} />
+    </Badge>
+  )
+}
