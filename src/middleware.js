551abf1254990b14a52ad0f15014f262d30bfd36
Teknofilia <116430893+Teknofilia@users.noreply.github.com>
2024-01-07 06:03:05 +0700

    fix: components login.jsx and register.jsx

--- src/middleware.js
+++ src/middleware.js
@@ -3,22 +3,22 @@ import * as jose from "jose";
 
 export default async function middleware(req) {
 
-  // const jwtSecret = process.env.JWT_SECRET;
-  // const encodedJwtSecret = new TextEncoder().encode(jwtSecret);
-  // const token = req.cookies.get("token")?.value;
+  const jwtSecret = process.env.JWT_SECRET;
+  const encodedJwtSecret = new TextEncoder().encode(jwtSecret);
+  const token = req.cookies.get("token")?.value;
 
-  // console.log(token, "middleware matcher dashboard")
-  // if (!token) {    
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
+  console.log(token, "middleware matcher dashboard")
+  if (!token) {    
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
-  // try {
-  //   await jose.jwtVerify(token, encodedJwtSecret);
-  //   return NextResponse.next();
-  // } catch (error) {
-  //   console.log({ error });
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
+  try {
+    await jose.jwtVerify(token, encodedJwtSecret);
+    return NextResponse.next();
+  } catch (error) {
+    console.log({ error });
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
 
   return NextResponse.next();
