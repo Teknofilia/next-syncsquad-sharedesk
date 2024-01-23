90dfeb18dce32e376a4a2b36fdcc79e390a952ab
Teknofilia <116430893+Teknofilia@users.noreply.github.com>
2024-01-23 22:23:49 +0700

    fix: loading in login, register and logout button

--- src/components/auth/components/register.jsx
+++ src/components/auth/components/register.jsx
@@ -3,50 +3,83 @@
 import { Button, Input } from "@nextui-org/react";
 import React from "react";
 import Link from "next/link";
+import { useState } from "react";
 
 export const Register = () => {
-  async function handleRegister(event) {
-    event.preventDefault(); // Ga akan nge refresh
-    const firstName = event.target.firstName.value;
-    const lastName = event.target.lastName.value;
-    const username = event.target.username.value;
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+	const [loading, setLoading] = useState(false);
 
-    const res = await fetch("/api/users/register", {
-      method: "POST",
-      body: JSON.stringify({ firstName, lastName, username, email, password }),
-    });
-    const data = await res.json();
-    console.log(data);
-  }
+	async function handleRegister(event) {
+		setLoading(true);
 
-  return (
-    <div className="w-[460px] space-y-8">
-      <div>
-        <h3>Register</h3>
-        <p>Please create an account</p>
-      </div>
-      <form onSubmit={handleRegister}>
-        <div className="space-y-2">
-          <div className="grid grid-cols-2 gap-2">
-            <Input name="firstName" placeholder="First Name" />
-            <Input name="lastName" placeholder="Last Name" />
-          </div>
-          <Input name="username" placeholder="Username" />
-          <Input name="email" placeholder="Email" />
-          <Input name="password" placeholder="Password" type="password" />
-          <Button color="primary" type="submit" className="w-full">
-            Register
-          </Button>
-          <h5 className="text-sm">
-            Already have an account?{" "}
-            <Link href="/login" className="text-primary">
-              Login
-            </Link>
-          </h5>
-        </div>
-      </form>
-    </div>
-  );
+		event.preventDefault(); // Ga akan nge refresh
+		const firstName = event.target.firstName.value;
+		const lastName = event.target.lastName.value;
+		const username = event.target.username.value;
+		const email = event.target.email.value;
+		const password = event.target.password.value;
+
+		const res = await fetch("/api/users/register", {
+			method: "POST",
+			body: JSON.stringify({ firstName, lastName, username, email, password }),
+		});
+		const data = await res.json();
+		console.log(data);
+		setLoading(false);
+	}
+
+	return (
+		<div className="w-[460px] space-y-8">
+			<div>
+				<h3>Register</h3>
+				<p>Please create an account</p>
+			</div>
+			<form onSubmit={handleRegister}>
+				<div className="space-y-2">
+					<div className="grid grid-cols-2 gap-2">
+						<Input name="firstName" placeholder="First Name" />
+						<Input name="lastName" placeholder="Last Name" />
+					</div>
+					<Input name="username" placeholder="Username" />
+					<Input name="email" placeholder="Email" />
+					<Input name="password" placeholder="Password" type="password" />
+					<Button
+						type="submit"
+						color="primary"
+						className="w-full"
+						isLoading={loading}
+						spinner={
+							<svg
+								className="animate-spin h-5 w-5 text-current"
+								fill="none"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<circle
+									className="opacity-25"
+									cx="12"
+									cy="12"
+									r="10"
+									stroke="currentColor"
+									strokeWidth="4"
+								/>
+								<path
+									className="opacity-75"
+									d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+									fill="currentColor"
+								/>
+							</svg>
+						}
+					>
+						Register
+					</Button>
+					<h5 className="text-sm">
+						Already have an account?{" "}
+						<Link href="/login" className="text-primary">
+							Login
+						</Link>
+					</h5>
+				</div>
+			</form>
+		</div>
+	);
 };
--- src/components/dashboard/components/dashboardTemplate.jsx
+++ src/components/dashboard/components/dashboardTemplate.jsx
@@ -5,49 +5,83 @@ import { Activity, Box, Receipt } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Button } from "@nextui-org/react";
 import Cookies from "js-cookie";
+import { useState } from "react";
 
 export const DashboardTemplate = ({ children }) => {
-  const router = useRouter();
+	const router = useRouter();
+	const [loading, setLoading] = useState(false);
 
-  async function handleSubmitLogout() {
-    Cookies.remove("token");
-    localStorage.removeItem("token");
-    localStorage.removeItem("userId");
+	async function handleSubmitLogout() {
+		setLoading(true);
 
-    let valToken = Cookies.get("token");
+		Cookies.remove("token");
+		localStorage.removeItem("token");
+		localStorage.removeItem("userId");
 
-    if (valToken) {
-      console.error("Error logout!");
-      return;
-    }
+		let valToken = Cookies.get("token");
 
-    setTimeout(() => router.push("/"), 2000);
-  }
+		if (valToken) {
+			console.error("Error logout!");
+			return;
+		}
+		setLoading(false);
 
-  return (
-    <main className="flex h-screen">
-      <aside className="w-[230px] border-r-2 p-8 flex flex-col justify-between">
-        <div>
-          <Link className="menu" href="/dashboard">
-            <Activity size={15} />
-            Dashboard
-          </Link>
-          <Link className="menu" href="/dashboard/products">
-            <Box size={15} />
-            Products
-          </Link>
-          <Link className="menu" href="/dashboard/orders">
-            <Receipt size={15} />
-            Orders
-          </Link>
-        </div>
-        <div className="menu">
-          <Button onClick={handleSubmitLogout}>Logout</Button>
-        </div>
-      </aside>
-      <section className="w-[calc(100vw-230px)] p-8">
-        <div className="max-w-5xl m-auto">{children}</div>
-      </section>
-    </main>
-  );
+		setTimeout(() => router.push("/"), 2000);
+	}
+
+	return (
+		<main className="flex h-screen">
+			<aside className="w-[230px] border-r-2 p-8 flex flex-col justify-between">
+				<div>
+					<Link className="menu" href="/dashboard">
+						<Activity size={15} />
+						Dashboard
+					</Link>
+					<Link className="menu" href="/dashboard/products">
+						<Box size={15} />
+						Products
+					</Link>
+					<Link className="menu" href="/dashboard/orders">
+						<Receipt size={15} />
+						Orders
+					</Link>
+				</div>
+				<div className="menu">
+					<Button
+						onClick={handleSubmitLogout}
+						color="warning"
+						className="w-full"
+						isLoading={loading}
+						spinner={
+							<svg
+								className="animate-spin h-5 w-5 text-current"
+								fill="none"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<circle
+									className="opacity-25"
+									cx="12"
+									cy="12"
+									r="10"
+									stroke="currentColor"
+									strokeWidth="4"
+								/>
+								<path
+									className="opacity-75"
+									d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+									fill="currentColor"
+								/>
+							</svg>
+						}
+					>
+						Logout
+					</Button>
+				</div>
+			</aside>
+			<section className="w-[calc(100vw-230px)] p-8">
+				<div className="max-w-5xl m-auto">{children}</div>
+			</section>
+		</main>
+	);
 };
