1df63b0e4a3c0d1a812506db4f8834fc34b95d32
Teknofilia <116430893+Teknofilia@users.noreply.github.com>
2024-01-28 19:56:41 +0700

    fix: images, red logout button, toast

--- src/app/(dashboard)/dashboard/[id]/page.js
+++ src/app/(dashboard)/dashboard/[id]/page.js
@@ -1,84 +1,98 @@
-"use client"
+"use client";
 
-import prisma from "@/utils/prisma"
+import { imageUrl } from "@/config/apiUrl";
+import prisma from "@/utils/prisma";
+import slugify from "slugify";
+
 //import ModalAddReservasi from '../../../../components/modal/modal-Add-Reservation'
 
 async function getProductDetail(idParam) {
-  const dataListProduct = await prisma.product_Listing.findMany({
-    where: {
-      id: {
-        contains: idParam || "",
-        mode: "insensitive",
-      },
-    },
-  });
+	const dataListProduct = await prisma.product_Listing.findMany({
+		where: {
+			id: {
+				contains: idParam || "",
+				mode: "insensitive",
+			},
+		},
+	});
 
-  return dataListProduct;
+	return dataListProduct;
 }
 
 export default async function DashboardDetailPage({ params }) {
-  const [data] = await getProductDetail(params.id)
-  // const [isShow, setIsShow] = useState(false)
-  // const [detail, setDetail] = useState(null)
-  // const [id, setId] = useState(false)
+	const [data] = await getProductDetail(params.id);
+	// const [isShow, setIsShow] = useState(false)
+	// const [detail, setDetail] = useState(null)
+	// const [id, setId] = useState(false)
 
-  // /** @param {string} id */
-  // const showModalUpdate = (id, data) => {
-  //   setId(id)
-  //   setDetail(data)
-  //   setIsShow(true)
-  // }
+	// /** @param {string} id */
+	// const showModalUpdate = (id, data) => {
+	//   setId(id)
+	//   setDetail(data)
+	//   setIsShow(true)
+	// }
 
-  // useEffect(()=>{
-  //   if(!isShow) {
-  //     setId("")
-  //     setDetail(null)
-  //   }
-  // },[isShow])
+	// useEffect(()=>{
+	//   if(!isShow) {
+	//     setId("")
+	//     setDetail(null)
+	//   }
+	// },[isShow])
 
-  return (
-    <>
-      {/* <ModalAddReservasi id={id} isShow={isShow} setIsShow={setIsShow} data={detail} /> */}
-      <div className="w-full h-full">
-        <div className="flex flex-wrap justify-center space-x-4 mb-6">
-          {data.images.map((value, index) => {
-            const pathImage = `https://sharedesk.s3.ap-southeast-1.amazonaws.com/products/${data.id}/${value}`
-            return (
-              <div key={index} className="mb-4">
-                <img
-                  className="rounded-lg"
-                  src={pathImage}
-                  alt=""
-                  height={400}
-                  width={450}
-                />
-              </div>
-            );
-          })}
-        </div>
-        <div className="p-2">
-          <h1 className="text-transform: capitalize">{data.name}</h1>
-        </div>
-        <div className="p-2">
-          <h5 className="font-light text-transform: normal-case">{data.description}</h5>
-        </div>
-        <div className="p-2">
-          <p className="text-lg font-semibold text-zinc-500">{data.guestCount} guests &#183; {data.roomCount} rooms</p>
-        </div>
-        <div className="p-2">
-          <button
-            color="primary"
-            role="button"
-            onClick={ () => showModalUpdate(data.id, data) }
-            className="
+	return (
+		<>
+			{/* <ModalAddReservasi id={id} isShow={isShow} setIsShow={setIsShow} data={detail} /> */}
+			<div className="w-full h-full">
+				<div className="flex flex-wrap justify-center space-x-4 mb-6">
+					{data.images.map((value, index) => {
+						// console.log(value);
+						// console.log(index);
+						// console.log(data.userId);
+						const value1 = slugify(value, { replacement: "+" });
+
+						// console.log(value1);
+
+						const pathImage = `${imageUrl}/${data.id}/${value1}`;
+						return (
+							<div key={index} className="mb-4">
+								<img
+									className="rounded-lg"
+									src={pathImage}
+									alt=""
+									height={400}
+									width={450}
+								/>
+							</div>
+						);
+					})}
+				</div>
+				<div className="p-2">
+					<h1 className="text-transform: capitalize">{data.name}</h1>
+				</div>
+				<div className="p-2">
+					<h5 className="font-light text-transform: normal-case">
+						{data.description}
+					</h5>
+				</div>
+				<div className="p-2">
+					<p className="text-lg font-semibold text-zinc-500">
+						{data.guestCount} guests &#183; {data.roomCount} rooms
+					</p>
+				</div>
+				<div className="p-2">
+					<button
+						color="primary"
+						role="button"
+						onClick={() => showModalUpdate(data.id, data)}
+						className="
             focus:ring-offset-2 leading-none text-white font-medium rounded-lg text-sm px-4 py-2
             border bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none 
             focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
-        </div>
-      </div>
-    </>
-  );
+					>
+						Submit
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
--- src/app/(dashboard)/dashboard/page.js
+++ src/app/(dashboard)/dashboard/page.js
@@ -1,75 +1,92 @@
-import InputSearchComponent from "./component/InputSearch"
-import prisma from "@/utils/prisma"
+import { imageUrl } from "@/config/apiUrl";
+import InputSearchComponent from "./component/InputSearch";
+import prisma from "@/utils/prisma";
+import slugify from "slugify";
 
-/** 
- * @param {string} name  
-*/
+/**
+ * @param {string} name
+ */
 async function getProducts(nameParam) {
+	// const dataListProduct = await fetch(`http://localhost:3000/api/products?name=${name}`, {
+	//   method: "GET",
+	//   next: { revalidate: 0 }
+	// })
 
-  // const dataListProduct = await fetch(`http://localhost:3000/api/products?name=${name}`, {
-  //   method: "GET",
-  //   next: { revalidate: 0 }
-  // })
+	// const responseDataListProduct = await dataListProduct.json()
 
-  // const responseDataListProduct = await dataListProduct.json()
+	// return responseDataListProduct.data
 
-  // return responseDataListProduct.data
+	const dataListProduct = await prisma.product_Listing.findMany({
+		where: {
+			name: {
+				contains: nameParam || "",
+				mode: "insensitive",
+			},
+		},
+	});
 
-  const dataListProduct = await prisma.product_Listing.findMany({
-    where: {
-      name: {
-        contains: nameParam || "",
-        mode: "insensitive",
-      },
-    },
-  });
-
-  return dataListProduct
-
+	return dataListProduct;
 }
 
-export default async function DashboardPage({searchParams}) {  
+export default async function DashboardPage({ searchParams }) {
+	const { name } = searchParams;
 
-  const { name } = searchParams
+	const data = await getProducts(name);
 
-  const data = await getProducts(name)
-
-  return (
-    <div className="">
-      <div className="mb-5">
-        <InputSearchComponent ></InputSearchComponent>
-      </div>
-      <div className=" grid gap-4 grid-cols-3 mb-10">
-        {
-          data?.map((value, index) => {
-
-            return (
-
-              <div key={index} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href={`/dashboard/${value.id}`}>
-                  <img className="rounded-t-lg" src={value.featuredImage} alt="" />
-                </a>
-                <div className="p-5">
-                  <a href={`/dashboard/${value.id}`}>
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      {value.name}
-                    </h5>
-                  </a>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    {value.description}
-                  </p>
-                  <a href={`/dashboard/${value.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                  </a>
-                </div>
-              </div>
-            )
-          })
-        }
-      </div>
-    </div>
-  )
+	return (
+		<div className="">
+			<div className="mb-5">
+				<InputSearchComponent></InputSearchComponent>
+			</div>
+			<div className=" grid gap-4 grid-cols-3 mb-10">
+				{data.map((value, index) => {
+					// const value1 = slugify(value.featuredImage, { replacement: "+" });
+					const pathImage = `${imageUrl}/${value.id}/${value.featuredImage}`;
+					// console.log(value.featuredImage);
+					// console.log(value.id);
+					return (
+						<div
+							key={index}
+							className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+						>
+							<a href={`/dashboard/${value.id}`}>
+								<img className="rounded-t-lg" src={pathImage} alt="" />
+							</a>
+							<div className="p-5">
+								<a href={`/dashboard/${value.id}`}>
+									<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+										{value.name}
+									</h5>
+								</a>
+								<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+									{value.description}
+								</p>
+								<a
+									href={`/dashboard/${value.id}`}
+									className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+								>
+									Read more
+									<svg
+										className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+										aria-hidden="true"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 14 10"
+									>
+										<path
+											stroke="currentColor"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
+											d="M1 5h12m0 0L9 1m4 4L9 9"
+										/>
+									</svg>
+								</a>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
